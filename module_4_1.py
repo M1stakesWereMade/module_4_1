6f6413442e4b2144a005f5b4dc917b2df6d59011
M1stakesWereMade <superlexlive@gmail.com>
2024-07-23 15:17:04 +0300

    module_4_1.py

--- module_4_1.py
+++ module_4_1.py
@@ -2,9 +2,9 @@ from fake_math import divide as fake_divide
 from true_math import divide as true_divide
 
 result1 = fake_divide(69, 3)
-result2 = fake_divide(3, 0)  # Ожидаемый вывод: 'Ошибка'
+result2 = fake_divide(3, 0)
 result3 = true_divide(49, 7)
-result4 = true_divide(15, 0)  # Ожидаемый вывод: inf
+result4 = true_divide(15, 0)
 
 print(result1)
 print(result2)
